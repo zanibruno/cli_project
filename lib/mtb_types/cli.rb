529e1d97371e0fb503d40f42da0a9bfacf6e7b25
brunozanin <corporate.zanin@gmail.com>
2019-10-09 12:25:46 +0100

    program finalized, everything works, maybe needs refactoring

--- lib/mtb_types/cli.rb
+++ lib/mtb_types/cli.rb
@@ -2,13 +2,13 @@
 class MtbTypes::CLI
 
   def call
-    puts "Hello and welcome to MTB Types:"
     list_types
     selection
     goodbye
   end
 
   def list_types
+    puts "Hello and welcome to MTB Types:"
 @names = MtbTypes::Types.all
 @names.each.with_index(1) do |types, i|
   puts "#{i} - #{types.name}."
@@ -19,18 +19,18 @@ class MtbTypes::CLI
   def selection
     input = nil
     while input != "exit"
-      puts "Enter a number to know more about the bikes, or type 'list' or 'exit'"
+      puts "Enter a number from 1 - 8 to know more about the bikes, or type list or exit"
       input = gets.strip.downcase
 
-      if input.to_i > 0
+      if input.to_i > 0 && input.to_i <= MtbTypes::Types.all.count
         the_types = @names[input.to_i-1]
         puts "#{the_types.description}"
       elsif input == "list"
         list_types
+      elsif input == "exit"
       else
-        puts "Not sure what you want type list or exit."
+        puts "Not sure what you want, enter a number from 1 - 8 or type list or exit."
       end
-
     end
   end
 
