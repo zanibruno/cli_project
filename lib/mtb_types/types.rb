5e2b4217b9a67ba0a370fbb3911ac4822a1c59a0
brunozanin <corporate.zanin@gmail.com>
2019-10-09 12:26:27 +0100

    code ready for interview, maybe needs to refactor

--- lib/mtb_types/types.rb
+++ lib/mtb_types/types.rb
@@ -7,7 +7,6 @@ class MtbTypes::Types
 
   attr_accessor :name, :description
 
-
 def self.all
   doc = Nokogiri::HTML(open("https://www.freethepowder.com/pages/mountain-bikes-what-are-the-different-types"))
   @names = doc.search(".content.container p strong")
@@ -26,23 +25,23 @@ type_3.name = doc.search(".content.container p strong")[2].text
 type_3.description = doc.search(".content.container p")[4].text
 
 type_4 = self.new
-type_4.name = doc.search(".content.container p strong")[3].text
+type_4.name = doc.search(".content.container p strong")[3].text.delete("1) ")
 type_4.description = doc.search(".content.container p")[7].text
 
 type_5 = self.new
-type_5.name = doc.search(".content.container span strong")[3].text
+type_5.name = doc.search(".content.container span strong")[3].text.delete("2) ")
 type_5.description = doc.search(".content.container span")[5].text
 
 type_6 = self.new
-type_6.name = doc.search(".content.container p strong")[4].text
+type_6.name = doc.search(".content.container p strong")[4].text.delete("3) ")
 type_6.description = doc.search(".content.container p")[12].text
 
 type_7 = self.new
-type_7.name = doc.search(".content.container p strong")[5].text
+type_7.name = doc.search(".content.container p strong")[5].text.delete("4) ")
 type_7.description = doc.search(".content.container p")[14].text
 
 type_8 = self.new
-type_8.name = doc.search(".content.container p strong")[6].text
+type_8.name = doc.search(".content.container p strong")[6].text.delete("5) ")
 type_8.description = doc.search(".content.container p")[16].text
 
 [type_1, type_2, type_3, type_4, type_5, type_6, type_7, type_8]
